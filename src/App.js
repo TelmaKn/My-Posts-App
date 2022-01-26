6c4462d80982fb2582ab2be4b5c6f5f730aa4a9f
Telma <telmakn@gmail.com>
2022-01-26 19:39:13 -0300

    installed packages and deleted folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./views/home/Home";
+import Login from "./views/login/Login";
+import Layout from "./components/layout/Layout";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">holu</header>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/Login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+
+            {/* <PrivateRoute exact path="/" component={Home} /> */}
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
